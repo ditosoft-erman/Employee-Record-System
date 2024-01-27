3e7121b14a28340628ac6f83bdf03a216496ecd8
Erman Louie Faminiano <119798034+ditosoft-erman@users.noreply.github.com>
2024-01-27 15:25:57 +0800

    Add files via upload

---
+++ src/main/java/com/myselfcode/employeesrecordsystem/EmployeesRecordSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myselfcode.employeesrecordsystem;
+
+import Authentication.LoginForm;
+
+/**
+ *
+ * @author Narra
+ */
+public class EmployeesRecordSystem {
+
+    public static void main(String[] args) {
+        LoginForm lf = new LoginForm();
+        lf.show();
+       
+    }
+}
